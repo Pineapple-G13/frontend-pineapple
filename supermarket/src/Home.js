4ae6b4de07ea61c8a7606b06d801f6a29883e331
hackerrank <adrianitajs09@hotmail.com>
2021-11-09 16:20:30 -0600

    vistas

---
+++ supermarket/src/Home.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="homeContainer">
+      <footer className="homeheader">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className="homeNombreProyecto">
+          <h1>Pineapple Supermarket App</h1>
+          <h2>Team G13</h2>
+          <h3>Developed by Adriana Jimenez, Shel Valdez, Valentina, Vilma Garcia </h3>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
