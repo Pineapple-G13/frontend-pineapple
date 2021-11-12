766e7ab94339c408c16bfef905fcca66c19ee02a
hackerrank <adrianitajs09@hotmail.com>
2021-11-12 15:48:19 -0600

    Multiple selector
    Se incorpora el area de seleccion de rol del usuario

--- Vista_newUser/form.js
+++ Vista_newUser/form.js
@@ -1,10 +1,26 @@
 var inputs = document.getElementsByClassName('formularioInput');
-for(var i = 0; i < inputs.length; i++){
-    inputs[i].addEventListener('keyup',function(){
-        if(this.value.length>=1){
-                this.nextElementSibling.classList.add('fijar');
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('keyup', function () {
+        if (this.value.length >= 1) {
+            this.nextElementSibling.classList.add('fijar');
         } else {
             this.nextElementSibling.classList.remove('fijar');
         }
     });
 }
+
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector(".opt").innerHTML;
+        optionsContainer.classList.remove("active");
+    });
+});
