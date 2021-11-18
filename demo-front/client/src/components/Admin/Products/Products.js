34f4f2b858c0b70a9da1ecfccfc2990997778ed5
hackerrank <adrianitajs09@hotmail.com>
2021-11-18 14:36:46 -0600

    Traduccion
    Traduje los campos de New Product

--- demo-front/client/src/components/Admin/Products/Products.js
+++ demo-front/client/src/components/Admin/Products/Products.js
@@ -1,116 +1,116 @@
-import React, {useState } from 'react'
+import React, { useState } from 'react'
 /* import axios from "axios"; */
 import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Products.css';
-import {productos} from './data'
+import { productos } from './data'
 
 const Products = () => {
 
-    const [data, setData] = useState(productos);
-    const [modalEditar, setModalEditar] = useState(false);
-    const [modalEliminar, setModalEliminar] = useState(false);
-    const [modalInsertar, setModalInsertar] = useState(false);
+  const [data, setData] = useState(productos);
+  const [modalEditar, setModalEditar] = useState(false);
+  const [modalEliminar, setModalEliminar] = useState(false);
+  const [modalInsertar, setModalInsertar] = useState(false);
 
-    const [productSeleccionado, setProductSeleccionado] = useState({
-        id: '',
-        nombre: '',
-        categoria: '',
-        descripcion:'',
-        cantidad:0,
-        precio_unitario:0,
-        picture:''
-        });
-        
-    const seleccionarProducto=(elemento, caso)=>{
-        setProductSeleccionado(elemento);
-        (caso==='Editar')?setModalEditar(true):setModalEliminar(true)
-    }
-        
-    const handleChange=e=>{
-        const {name, value}=e.target;
-        setProductSeleccionado((prevState)=>({
-        ...prevState,
-        [name]: value
-        }));
-    }
-        
-    const editar=()=>{
-        var dataNueva=data;
-        // eslint-disable-next-line array-callback-return
-        dataNueva.map((prod) =>{
-            if(prod.id ===productSeleccionado.id){
-                prod.nombre=productSeleccionado.nombre;
-                prod.categoria=productSeleccionado.categoria;
-                prod.descripcion=productSeleccionado.descripcion;
-                prod.cantidad=productSeleccionado.cantidad;
-                prod.precio_unitario=productSeleccionado.precio_unitario;
-                prod.picture=productSeleccionado.picture;
-            }
-        });
-        setData(dataNueva);
-        setModalEditar(false);
-    }
-        
-          const eliminar =()=>{
-            setData(data.filter(prod=>prod.id!==productSeleccionado.id));
-            setModalEliminar(false);
+  const [productSeleccionado, setProductSeleccionado] = useState({
+    id: '',
+    nombre: '',
+    categoria: '',
+    descripcion: '',
+    cantidad: 0,
+    precio_unitario: 0,
+    picture: ''
+  });
+
+  const seleccionarProducto = (elemento, caso) => {
+    setProductSeleccionado(elemento);
+    (caso === 'Editar') ? setModalEditar(true) : setModalEliminar(true)
+  }
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setProductSeleccionado((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
+
+  const editar = () => {
+    var dataNueva = data;
+    // eslint-disable-next-line array-callback-return
+    dataNueva.map((prod) => {
+      if (prod.id === productSeleccionado.id) {
+        prod.nombre = productSeleccionado.nombre;
+        prod.categoria = productSeleccionado.categoria;
+        prod.descripcion = productSeleccionado.descripcion;
+        prod.cantidad = productSeleccionado.cantidad;
+        prod.precio_unitario = productSeleccionado.precio_unitario;
+        prod.picture = productSeleccionado.picture;
+      }
+    });
+    setData(dataNueva);
+    setModalEditar(false);
+  }
+
+  const eliminar = () => {
+    setData(data.filter(prod => prod.id !== productSeleccionado.id));
+    setModalEliminar(false);
+  }
+
+  const abrirModalInsertar = () => {
+    setProductSeleccionado(null);
+    setModalInsertar(true);
+  }
+
+  const insertar = () => {
+    var valorInsertar = productSeleccionado;
+    valorInsertar.id = data[data.length - 1].id + 1;
+    var dataNueva = data;
+    dataNueva.push(valorInsertar);
+    setData(dataNueva);
+    setModalInsertar(false);
+  }
+
+  return (
+    <div className="container">
+      <h2 className="paddingH2">Productos</h2>
+      <br />
+      <button className="btn btn-success" onClick={() => abrirModalInsertar()}>New Product</button>
+      <br /><br />
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Categoria</th>
+            <th>Descripcion</th>
+            <th>Cantidad</th>
+            <th>Precio Unitario</th>
+            <th>Picture</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((elemento) => (
+
+            <tr key={elemento.id}>
+              <td>{elemento.id}</td>
+              <td>{elemento.nombre}</td>
+              <td>{elemento.categoria}</td>
+              <td>{elemento.descripcion}</td>
+              <td>{elemento.cantidad}</td>
+              <td>{elemento.precio_unitario}</td>
+              <td><img src={`${process.env.PUBLIC_URL}/img/${elemento.picture}`} alt={elemento.nombre} width="50px" className="img-fluid" /></td>
+              <td><button className="btnEdit" onClick={() => seleccionarProducto(elemento, 'Editar')} ><i className="fas fa-edit"></i></button>
+                <button className="btnEdit" onClick={() => seleccionarProducto(elemento, 'Eliminar')}><i className="fas fa-trash-alt"></i></button>
+              </td>
+            </tr>
+          ))
           }
-        
-          const abrirModalInsertar=()=>{
-            setProductSeleccionado(null);
-            setModalInsertar(true);
-          }
-        
-          const insertar =()=>{
-            var valorInsertar=productSeleccionado;
-            valorInsertar.id=data[data.length-1].id+1;
-            var dataNueva = data;
-            dataNueva.push(valorInsertar);
-            setData(dataNueva);
-            setModalInsertar(false);
-          }
-        
-        return (
-            <div className="container">
-                <h2 className="paddingH2">Productos</h2>
-                <br />
-                <button className="btn btn-success" onClick={()=>abrirModalInsertar()}>New Product</button>
-                <br /><br />
-                <table className="table table-bordered">
-                    <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Categoria</th>
-                        <th>Descripcion</th>
-                        <th>Cantidad</th>
-                        <th>Precio Unitario</th>
-                        <th>Picture</th>
-                        <th>Acciones</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {data.map((elemento)=>(
-                      
-                        <tr key={elemento.id}>
-                          <td>{elemento.id}</td>
-                          <td>{elemento.nombre}</td>
-                          <td>{elemento.categoria}</td>
-                          <td>{elemento.descripcion}</td>
-                          <td>{elemento.cantidad}</td>
-                          <td>{elemento.precio_unitario}</td>
-                          <td><img src={`${process.env.PUBLIC_URL}/img/${elemento.picture}`} alt={elemento.nombre} width="50px" className="img-fluid"/></td>
-                          <td><button className="btnEdit" onClick={()=>seleccionarProducto(elemento, 'Editar')} ><i className="fas fa-edit"></i></button>
-                              <button className="btnEdit" onClick={()=>seleccionarProducto(elemento, 'Eliminar')}><i className="fas fa-trash-alt"></i></button>
-                          </td>
-                        </tr>
-                    ))
-                    }
-                    {console.log(data)}
-                    </tbody>
-                </table>
-                <Modal isOpen={modalEditar}>
+          {console.log(data)}
+        </tbody>
+      </table>
+      <Modal isOpen={modalEditar}>
         <ModalHeader>
           <div>
             <h3>Editar Producto</h3>
@@ -181,12 +181,12 @@ const Products = () => {
           </div>
         </ModalBody>
         <ModalFooter>
-          <button className="btn btn-primary" onClick={()=>editar()}>
+          <button className="btn btn-primary" onClick={() => editar()}>
             Actualizar
           </button>
           <button
             className="btn btn-danger"
-            onClick={()=>setModalEditar(false)}
+            onClick={() => setModalEditar(false)}
           >
             Cancelar
           </button>
@@ -199,12 +199,12 @@ const Products = () => {
           Estás Seguro que deseas eliminar el producto {productSeleccionado && productSeleccionado.nombre}
         </ModalBody>
         <ModalFooter>
-          <button className="btn btn-danger" onClick={()=>eliminar()}>
+          <button className="btn btn-danger" onClick={() => eliminar()}>
             Sí
           </button>
           <button
             className="btn btn-secondary"
-            onClick={()=>setModalEliminar(false)}
+            onClick={() => setModalEliminar(false)}
           >
             No
           </button>
@@ -212,10 +212,10 @@ const Products = () => {
       </Modal>
 
 
-        <Modal isOpen={modalInsertar}>
+      <Modal isOpen={modalInsertar}>
         <ModalHeader>
           <div>
-            <h3>Insertar Producto</h3>
+            <h3>Insert Product</h3>
           </div>
         </ModalHeader>
         <ModalBody>
@@ -226,35 +226,35 @@ const Products = () => {
               readOnly
               type="text"
               name="id"
-              value={data[data.length-1].id+1}
+              value={data[data.length - 1].id + 1}
             />
             <br />
 
-            <label>Nombre</label>
+            <label>Name</label>
             <input
               className="form-control"
               type="text"
               name="nombre"
-              value={productSeleccionado ? productSeleccionado.nombre: ''}
+              value={productSeleccionado ? productSeleccionado.nombre : ''}
               onChange={handleChange}
             />
             <br />
 
-            <label>Categoria</label>
+            <label>Category</label>
             <input
               className="form-control"
               type="text"
               name="categoria"
-              value={productSeleccionado ? productSeleccionado.categoria: ''}
+              value={productSeleccionado ? productSeleccionado.categoria : ''}
               onChange={handleChange}
             />
             <br />
-            <label>Descripcion</label>
+            <label>Description</label>
             <input
               className="form-control"
               type="text"
               name="categoria"
-              value={productSeleccionado ? productSeleccionado.descripcion: ''}
+              value={productSeleccionado ? productSeleccionado.descripcion : ''}
               onChange={handleChange}
             />
             <br />
@@ -262,19 +262,19 @@ const Products = () => {
         </ModalBody>
         <ModalFooter>
           <button className="btn btn-primary"
-          onClick={()=>insertar()}>
-            Insertar
+            onClick={() => insertar()}>
+            Insert
           </button>
           <button
             className="btn btn-danger"
-            onClick={()=>setModalInsertar(false)}
+            onClick={() => setModalInsertar(false)}
           >
-            Cancelar
+            Cancel
           </button>
         </ModalFooter>
       </Modal>
-            </div>
-        )
-    
+    </div>
+  )
+
 }
 export default Products
